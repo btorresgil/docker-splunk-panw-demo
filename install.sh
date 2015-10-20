c9ca3d21c77ad6fe67c8d2c3f3de4489ce76cd73
Brian Torres-Gil <btorres-gil@paloaltonetworks.com>
2015-10-20 10:27:54 -0700

    Check if upgrade or new install

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/sh
 
 ##
 ## How to use this installer (and upgrader)
@@ -7,10 +7,20 @@
 # Check that docker is installed
 command -v docker >/dev/null 2>&1 || { echo >&2 "Docker is not installed yet.  Aborting."; exit 1; }
 
+# Check if a system-local directory exists
+# This determines if fresh install or existing install
+if [ -d "$PWD/system-local" ]; then
+    echo "Found existing Splunk config in local directory. Performing upgrade."
+    UPGRADE="-e SPUNK_EXISTING_CONFIG=true"
+else
+    echo "No existing Splunk config. Performing fresh install."
+    UPGRADE=""
+fi
+
 # Create a blank license file if none exists
 touch $PWD/license.lic
 
-# Shut down any current splunk-demo
+# Destroy any current splunk-demo
 docker rm -fv splunk-demo >/dev/null 2>&1
 
 # docker pull latest version of demo
@@ -20,4 +30,16 @@ docker pull btorresgil/splunk-panw-demo
 docker rmi $(docker images -f "dangling=true" -q) >/dev/null 2>&1
 
 # docker run w/ many volumes in the local directory
-docker run -d --name splunk-demo -p 8000:8000 -p 514:514/udp -e SPLUNK_PW=paloalto -v $PWD/license.lic:/license.lic -v $PWD/db:/opt/splunk/var/lib/splunk -v $PWD/system-local:/opt/splunk/etc/system/local -v $PWD/users-local:/opt/splunk/etc/users -v $PWD/app-local:/opt/splunk/etc/apps/SplunkforPaloAltoNetworks/local btorresgil/splunk-panw-demo
+docker run -d --name splunk-demo \
+    -e SPLUNK_PW=paloalto \
+    ${UPGRADE} \
+    -p 8000:8000 \
+    -p 514:514/udp \
+    -p 514:514 \
+    -v $PWD/license.lic:/license.lic \
+    -v $PWD/db:/opt/splunk/var/lib/splunk \
+    -v $PWD/system-local:/opt/splunk/etc/system/local \
+    -v $PWD/users-local:/opt/splunk/etc/users \
+    -v $PWD/app-local:/opt/splunk/etc/apps/SplunkforPaloAltoNetworks/local \
+    btorresgil/splunk-panw-demo
+    -v /etc/localtime:/etc/localtime:ro \
