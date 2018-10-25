9fbf72fc09d8857cec34e38536cb82aa4f314ae0
Paul Nguyen <panguyen@paloaltonetworks.com>
2018-10-25 15:05:45 -0700

    Entry point copies app, add-on, eventgen. New messages to confirm it.

--- demo_entrypoint.sh
+++ demo_entrypoint.sh
@@ -13,39 +13,7 @@ if [ "$1" = 'start-service' ]; then
 
     cp -fR /var/opt/splunk/etc ${SPLUNK_HOME}
 
-    # Upgrade the Palo Alto Networks app, even if a config exists already
-    # Delete everything but the local directory itself
-    find ${APP_HOME}/* -type d ! -name local -exec rm -rf {} + &2>1 > /dev/null
-    rm -f ${APP_HOME}/*
-    find /opt/splunk/etc/apps
-    echo "Making Folder"
-    mkdir /opt/splunk/etc/apps/SplunkforPaloAltoNetworks
-    cp -fvR /panw-apps/SplunkforPaloAltoNetworks ${APP_HOME}
-    echo "Find App in SPLUNK"
-    find /opt/splunk/etc/apps/SplunkforPaloAltoNetworks/*
-
-    # Upgrade the Splunk_TA_paloalto Add-on, even if a config exists already
-    # Delete everything but the local directory itself
-    find ${ADDON_HOME}/* -type d ! -name local -exec rm -rf {} + &2>1 > /dev/null
-    rm -f ${ADDON_HOME}/*
-    cp -fR /panw-apps/Splunk_TA_paloalto ${APPS_HOME}
-    # echo "Find Add-on in SPLUNK"
-    # find /opt/splunk/etc/apps/Splunk_TA_paloalto/*
-
-    # Upgrade the SA-Eventgen app, even if a config exists already
-    # Delete everything but the local directory itself
-    find ${DATAGEN_HOME}/* -type d ! -name local -exec rm -rf {} + &2>1 > /dev/null
-    rm -f ${DATAGEN_HOME}/*
-    cp -fR /panw-apps/SA-Eventgen ${APPS_HOME}
-    if [ ! -f ${DATAGEN_HOME}/local/app.conf ]; then
-      echo -e "[ui]\nis_visible = false\n" > ${DATAGEN_HOME}/local/app.conf
-    fi
-
-    # Copy the default inputs.conf for the Palo Alto Networks App, if one
-    # doesn't exist already.
-    cp -n /inputs.conf ${APP_HOME}/local/inputs.conf
-    rm -f /inputs.conf
-
+    
     if [ ! -f ${SPLUNK_HOME}/etc/system/local/web.conf ]; then
       mkdir -p ${SPLUNK_HOME}/etc/system/local
       echo -e "[default]\n\n[settings]\n" >> ${SPLUNK_HOME}/etc/system/local/web.conf
@@ -55,6 +23,44 @@ if [ "$1" = 'start-service' ]; then
       mkdir -p ${APPS_HOME}/splunk_instrumentation/local
       echo -e "[general]\nsendAnonymizedUsage = 0\nsendLicenseUsage = 0\nshowOptInModal = 0" > ${APPS_HOME}/splunk_instrumentation/local/telemetry.conf
     fi
+
+    # Upgrade the Palo Alto Networks app, even if a config exists already
+    # Delete everything but the local directory itself
+    find ${APP_HOME}/* -type d ! -name local -exec rm -rf {} + &2>1 > /dev/null
+    rm -f ${APP_HOME}/*
+    cp -fR /panw-apps/SplunkforPaloAltoNetworks ${APPS_HOME}
+    if [ -d "${APP_HOME}" ]; then
+      echo "SplunkforPaloAltoNetworks Copied"
+    fi
+
+    # Upgrade the Splunk_TA_paloalto Add-on, even if a config exists already
+    # Delete everything but the local directory itself
+    find ${ADDON_HOME}/* -type d ! -name local -exec rm -rf {} + &2>1 > /dev/null
+    rm -f ${ADDON_HOME}/*
+    cp -fR /panw-apps/Splunk_TA_paloalto ${APPS_HOME}
+    if [ -d "${ADDON_HOME}" ]; then
+      echo "Splunk_TA_paloalto Copied"
+    fi
+
+    # Upgrade the SA-Eventgen app, even if a config exists already
+    # Delete everything but the local directory itself
+    find ${DATAGEN_HOME}/* -type d ! -name local -exec rm -rf {} + &2>1 > /dev/null
+    rm -f ${DATAGEN_HOME}/*
+    cp -fR /panw-apps/SA-Eventgen ${APPS_HOME}
+    if [ ! -f ${DATAGEN_HOME}/local/app.conf ]; then
+      echo -e "[ui]\nis_visible = false\n" > ${DATAGEN_HOME}/local/app.conf
+    fi
+    if [ -d "${DATAGEN_HOME}" ]; then
+      echo "SA-Eventgen Copied"
+    fi
+
+
+
+    # Copy the default inputs.conf for the Palo Alto Networks App, if one
+    # doesn't exist already.
+    cp -n /inputs.conf ${APP_HOME}/local/inputs.conf
+    rm -f /inputs.conf
+
 
     touch etc/.ui_login # Disable the "first time signing in" message
 
@@ -80,4 +86,3 @@ export SPLUNK_PASSWORD="paloalto"
 
 /sbin/entrypoint.sh $@
 
-sudo -HEu ${SPLUNK_USER} sh -c "${SPLUNK_HOME}/bin/splunk edit user admin -password paloalto -auth admin:changeme"
