4edcc41e458d1333320c92b10ed351e3220ba2df
Brian Torres-Gil <brian@ixi.us>
2020-04-03 11:11:43 -0700

    fix: Uses latest Splunk docker with Ansible

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,14 @@
-FROM splunk/splunk:7.2.4
+FROM splunk/splunk:8.0.2.1
+LABEL authors "Brian Torres-Gil <brian@ixi.us>,Paul Nguyen <panguyen@paloaltonetworks.com>"
 
-MAINTAINER Brian Torres-Gil <btorresgil@dralth.com>
-
+# Versions
 ENV SPLUNK_USER root
-ENV REFRESHED_AT 2018-10-05
-ENV APP_VERSION 6.1.1
-ENV ADDON_VERSION 6.1.1 
+ENV REFRESHED_AT 2020-04-03
+ENV APP_VERSION 6.2.0
+ENV ADDON_VERSION 6.2.0 
 ENV EVENTGEN_VERSION develop
 
 USER root
-
-RUN apt-get update && apt-get install -y wget
-
 RUN mkdir /panw-apps
 
 # Download the latest stable Palo Alto Networks App for Splunk
@@ -20,12 +17,21 @@ RUN wget -qO /SplunkforPaloAltoNetworks.tar.gz https://github.com/PaloAltoNetwor
 RUN tar -xvf /SplunkforPaloAltoNetworks.tar.gz -C /panw-apps/
 RUN mv /panw-apps/SplunkforPaloAltoNetworks-${APP_VERSION} /panw-apps/SplunkforPaloAltoNetworks
 RUN rm -f /SplunkforPaloAltoNetworks.tar.gz
+RUN mkdir /panw-apps/SplunkforPaloAltoNetworks/local
+COPY datamodels.conf /panw-apps/SplunkforPaloAltoNetworks/local/datamodels.conf
+COPY inputs.conf /panw-apps/SplunkforPaloAltoNetworks/local/inputs.conf
+RUN cd /panw-apps; tar czvf /panw-apps/SplunkforPaloAltoNetworks.tgz SplunkforPaloAltoNetworks
 
 # Download the latest stable Palo Alto Networks Add-on for Splunk
 RUN wget -qO /Splunk_TA_paloalto.tar.gz https://github.com/PaloAltoNetworks/Splunk_TA_paloalto/archive/${ADDON_VERSION}.tar.gz
 RUN tar -xzf /Splunk_TA_paloalto.tar.gz -C /panw-apps/
 RUN mv /panw-apps/Splunk_TA_paloalto-${ADDON_VERSION} /panw-apps/Splunk_TA_paloalto
 RUN rm -f /Splunk_TA_paloalto.tar.gz
+COPY samples /panw-apps/Splunk_TA_paloalto/samples
+COPY files/eventgen.conf /panw-apps/Splunk_TA_paloalto/default/eventgen.conf
+COPY files/eventgen.conf.spec /panw-apps/Splunk_TA_paloalto/README/eventgen.conf.spec
+COPY files/eventgen_kvstore_loader.py /panw-apps/Splunk_TA_paloalto/bin/eventgen_kvstore_loader.py
+RUN cd /panw-apps; tar czvf /panw-apps/Splunk_TA_paloalto.tgz Splunk_TA_paloalto
 
 # Download the latest stable Eventgen
 RUN wget -qO /eventgen.tar.gz https://github.com/btorresgil/eventgen/archive/${EVENTGEN_VERSION}.tar.gz
@@ -33,28 +39,25 @@ RUN wget -qO /eventgen.tar.gz https://github.com/btorresgil/eventgen/archive/${E
 RUN tar -xzf /eventgen.tar.gz -C /panw-apps/
 RUN mv /panw-apps/eventgen-${EVENTGEN_VERSION} /panw-apps/SA-Eventgen
 RUN rm -f /eventgen.tar.gz
+RUN cd /panw-apps; tar czvf /panw-apps/eventgen.tgz SA-Eventgen
 
 # Add 514/udp syslog input to app
-RUN mkdir /panw-apps/SplunkforPaloAltoNetworks/local
 RUN mkdir /panw-apps/Splunk_TA_paloalto/local
-RUN mkdir /panw-apps/SA-Eventgen/local
-COPY inputs.conf /inputs.conf
-COPY datamodels.conf /datamodels.conf
-
-RUN apt-get purge -y --auto-remove wget
 
 COPY demo_entrypoint.sh /sbin/demo_entrypoint.sh
 RUN chmod +x /sbin/demo_entrypoint.sh
-
-COPY samples /panw-apps/Splunk_TA_paloalto/samples
-COPY files/eventgen.conf /panw-apps/Splunk_TA_paloalto/default/eventgen.conf
-COPY files/eventgen.conf.spec /panw-apps/Splunk_TA_paloalto/README/eventgen.conf.spec
-COPY files/eventgen_kvstore_loader.py /panw-apps/Splunk_TA_paloalto/bin/eventgen_kvstore_loader.py
 
 # Ports Splunk Web, Splunk Daemon, KVStore, Splunk Indexing Port, Network Input, HTTP Event Collector
 EXPOSE 8000/tcp 8089/tcp 8191/tcp 9997/tcp 1514 8088/tcp
 
 WORKDIR /opt/splunk
+
+# Splunk configuration
+ENV SPLUNK_HOME /opt/splunk
+ENV SPLUNK_START_ARGS --accept-license
+ENV SPLUNK_PASSWORD paloalto
+# ENV SPLUNK_LICENSE_URI Free
+ENV SPLUNK_APPS_URL "/panw-apps/SplunkforPaloAltoNetworks.tgz,/panw-apps/Splunk_TA_paloalto.tgz,/panw-apps/eventgen.tgz"
 
 # Configurations folder, var folder for everything (indexes, logs, kvstore)
 VOLUME [ "/opt/splunk/etc", "/opt/splunk/var" ]
--- demo_entrypoint.sh
+++ demo_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export APPS_HOME=/opt/splunk/etc/apps
+export APPS_HOME=${SPLUNK_HOME}/etc/apps
 export APP_HOME=$APPS_HOME/SplunkforPaloAltoNetworks
 export ADDON_HOME=$APPS_HOME/Splunk_TA_paloalto
 export DATAGEN_HOME=$APPS_HOME/SA-Eventgen
@@ -9,88 +9,85 @@ export DATAGEN_HOME=$APPS_HOME/SA-Eventgen
 if [ "$1" = 'start-service' ]; then
   # If these files are different override etc folder (possible that this is upgrade or first start cases)
   # Also override ownership of these files to splunk:splunk
-  if ! $(cmp --silent /var/opt/splunk/etc/splunk.version ${SPLUNK_HOME}/etc/splunk.version); then
+  # if ! cmp --silent /var/opt/splunk/etc/splunk.version "${SPLUNK_HOME}/etc/splunk.version"; then
 
-    cp -fR /var/opt/splunk/etc ${SPLUNK_HOME}
-
-    
-    if [ ! -f ${SPLUNK_HOME}/etc/system/local/web.conf ]; then
-      mkdir -p ${SPLUNK_HOME}/etc/system/local
-      echo -e "[default]\n\n[settings]\n" >> ${SPLUNK_HOME}/etc/system/local/web.conf
-      echo "updateCheckerBaseURL = 0" >> ${SPLUNK_HOME}/etc/system/local/web.conf
+    if [ ! -f "${SPLUNK_HOME}/etc/system/local/web.conf" ]; then
+      mkdir -p "${SPLUNK_HOME}/etc/system/local"
+      echo -e "[default]\n\n[settings]\n" >> "${SPLUNK_HOME}/etc/system/local/web.conf"
+      echo "updateCheckerBaseURL = 0" >> "${SPLUNK_HOME}/etc/system/local/web.conf"
     fi
-    if [ ! -f ${APPS_HOME}/splunk_instrumentation/local/telemetry.conf ]; then
-      mkdir -p ${APPS_HOME}/splunk_instrumentation/local
-      echo -e "[general]\nsendAnonymizedUsage = 0\nsendLicenseUsage = 0\nshowOptInModal = 0" > ${APPS_HOME}/splunk_instrumentation/local/telemetry.conf
+    if [ ! -f "${APPS_HOME}/splunk_instrumentation/local/telemetry.conf" ]; then
+      mkdir -p "${APPS_HOME}/splunk_instrumentation/local"
+      echo -e "[general]\nsendAnonymizedUsage = 0\nsendLicenseUsage = 0\nshowOptInModal = 0" > "${APPS_HOME}/splunk_instrumentation/local/telemetry.conf"
     fi
 
     # Upgrade the Palo Alto Networks app, even if a config exists already
     # Delete everything but the local directory itself
-    find ${APP_HOME}/* -type d ! -name local -exec rm -rf {} + &2>1 > /dev/null
-    rm -f ${APP_HOME}/*
-    cp -fR /panw-apps/SplunkforPaloAltoNetworks ${APPS_HOME}
-    if [ -d "${APP_HOME}" ]; then
-      echo "SplunkforPaloAltoNetworks Copied"
-    fi
+    # find ${APP_HOME}/* -type d ! -name local -exec rm -rf {} + &2>1 > /dev/null
+    # rm -f ${APP_HOME}/*
+    # cp -fR /panw-apps/SplunkforPaloAltoNetworks ${APPS_HOME}
+    # if [ -d "${APP_HOME}" ]; then
+    #   echo "SplunkforPaloAltoNetworks Copied"
+    # fi
 
     # Upgrade the Splunk_TA_paloalto Add-on, even if a config exists already
     # Delete everything but the local directory itself
-    find ${ADDON_HOME}/* -type d ! -name local -exec rm -rf {} + &2>1 > /dev/null
-    rm -f ${ADDON_HOME}/*
-    cp -fR /panw-apps/Splunk_TA_paloalto ${APPS_HOME}
-    if [ -d "${ADDON_HOME}" ]; then
-      echo "Splunk_TA_paloalto Copied"
-    fi
+    # find ${ADDON_HOME}/* -type d ! -name local -exec rm -rf {} + &2>1 > /dev/null
+    # rm -f ${ADDON_HOME}/*
+    # cp -fR /panw-apps/Splunk_TA_paloalto ${APPS_HOME}
+    # if [ -d "${ADDON_HOME}" ]; then
+    #   echo "Splunk_TA_paloalto Copied"
+    # fi
 
     # Upgrade the SA-Eventgen app, even if a config exists already
     # Delete everything but the local directory itself
-    find ${DATAGEN_HOME}/* -type d ! -name local -exec rm -rf {} + &2>1 > /dev/null
-    rm -f ${DATAGEN_HOME}/*
-    cp -fR /panw-apps/SA-Eventgen ${APPS_HOME}
-    if [ ! -f ${DATAGEN_HOME}/local/app.conf ]; then
-      echo -e "[ui]\nis_visible = false\n" > ${DATAGEN_HOME}/local/app.conf
-    fi
-    if [ -d "${DATAGEN_HOME}" ]; then
-      echo "SA-Eventgen Copied"
-    fi
+    # find ${DATAGEN_HOME}/* -type d ! -name local -exec rm -rf {} + &2>1 > /dev/null
+    # rm -f ${DATAGEN_HOME}/*
+    # cp -fR /panw-apps/SA-Eventgen ${APPS_HOME}
+    # if [ ! -f ${DATAGEN_HOME}/local/app.conf ]; then
+    #   echo -e "[ui]\nis_visible = false\n" > ${DATAGEN_HOME}/local/app.conf
+    # fi
+    # if [ -d "${DATAGEN_HOME}" ]; then
+    #   echo "SA-Eventgen Copied"
+    # fi
 
 
 
     # Copy the default inputs.conf for the Palo Alto Networks App, if one
     # doesn't exist already.
-    cp -n /inputs.conf ${APP_HOME}/local/inputs.conf
-    rm -f /inputs.conf
+    # mkdir ${SPLUNK_HOME}/etc/local
+    # cp -n /inputs.conf ${SPLUNK_HOME}/etc/local/inputs.conf
+    # rm -f /inputs.conf
 
     # Copy the datamodel.conf for the Palo Alto Networks App, if one
     # doesn't exist already.
-    cp -n /datamodels.conf ${APP_HOME}/local/datamodels.conf
-    rm -f /datamodels.conf
-    if [ -f "${APP_HOME}/local/datamodels.conf" ]; then
-      echo "DataModel Copied"
-    fi
+    # cp -n /datamodels.conf ${APP_HOME}/local/datamodels.conf
+    # rm -f /datamodels.conf
+    # if [ -f "${APP_HOME}/local/datamodels.conf" ]; then
+    #   echo "DataModel Copied"
+    # fi
 
 
-    touch etc/.ui_login # Disable the "first time signing in" message
+    # touch etc/.ui_login # Disable the "first time signing in" message
 
-    chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_HOME}/etc
-    chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_HOME}/var
+    # chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_HOME}/etc
+    # chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} ${SPLUNK_HOME}/var
 
     # Add data generator user
-    #sudo -HEu ${SPLUNK_USER} sh -c "${SPLUNK_HOME}/bin/splunk add user pan -password pan -role pan -auth admin:changeme --accept-license --answer-yes"
+    # sudo -HEu "${SPLUNK_USER}" sh -c "${SPLUNK_HOME}/bin/splunk add user pan -password pan -role pan -auth admin:changeme --accept-license --answer-yes"
 
-    if [ -e /license.lic ]; then
-      echo "Installing license"
-      sudo -HEu ${SPLUNK_USER} sh -c "${SPLUNK_HOME}/bin/splunk add licenses /license.lic --accept-license"
-    else
-      echo "Setting to free demo mode"
-      sudo -HEu ${SPLUNK_USER} sh -c "${SPLUNK_HOME}/bin/splunk edit licenser-groups Free -is_active 1 --accept-license"
-    fi
+    # if [ -e /license.lic ]; then
+    #   echo "Installing license"
+    #   sudo -HEu ${SPLUNK_USER} sh -c "${SPLUNK_HOME}/bin/splunk add licenses /license.lic --accept-license"
+    # else
+    #   echo "Setting to free demo mode"
+    #   sudo -HEu ${SPLUNK_USER} sh -c "${SPLUNK_HOME}/bin/splunk edit licenser-groups Free -is_active 1 --accept-license"
+    # fi
 
-  fi
+  # fi
 fi
 
-export SPLUNK_START_ARGS="${SPLUNK_START_ARGS} --accept-license"
-export SPLUNK_PASSWORD="paloalto"
+# export SPLUNK_START_ARGS="${SPLUNK_START_ARGS} --accept-license"
+# export SPLUNK_PASSWORD="paloalto"
 
 /sbin/entrypoint.sh $@
-
